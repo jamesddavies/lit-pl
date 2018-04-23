ec247cffd8483a05d7934be32e9be2b4444ad322
James Davies <jamesdaviddavies@gmail.com>
2018-04-23 20:41:57 +0100

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,13 @@
 var gulp = require('gulp')
-var minify = require('gulp-minify')
+var uglify = require('gulp-uglify-es').default
 var gzip = require('gulp-gzip')
 
-gulp.task('compress', function() {
-    gulp.src('src/**/*.js')
-        .pipe(minify({
-            ext:{
-                src:'-min.js',
-                min:'.js'
-            }
-        }))
-        .pipe(gulp.dest(''))
+gulp.task('uglify', function(cb) {  
+    return gulp.src('src/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('/'));
 })
 
 gulp.task('default', function() {
-    gulp.run('compress')
+    gulp.run('uglify')
 })
--- index.js
+++
@@ -1,1 +0,0 @@
-import{html,render}from"./node_modules/lit-html/lit-html.js";import{Router}from"./node_modules/lit-route/index.js";import App from"./components/app.component.js";const appRoot=document.getElementById("applicationRoot"),router=new Router(()=>render(App(),appRoot),appRoot);router.init();
