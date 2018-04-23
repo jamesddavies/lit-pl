4660256dbe4805a350be8158160b8238b4f9762f
James Davies <jamesdaviddavies@gmail.com>
2018-04-23 21:39:38 +0100

    updating lit-route and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,7 @@
 var gulp = require('gulp')
 var uglify = require('gulp-uglify-es').default
-var gzip = require('gulp-gzip')
 
-gulp.task('uglify', function(cb) {  
+gulp.task('uglify', function() {  
     return gulp.src('./src/**/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('./'));
